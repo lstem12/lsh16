9f855cbf15b1ec728faf024bb44261336951b54a
Host <Host@DESKTOP-L9VLQI0>
2020-06-16 19:10:07 +0900

    2020.06.16일 복습

--- src/lsh16/Lsh16.java
+++ src/lsh16/Lsh16.java
@@ -1,8 +1,45 @@
 package lsh16;
 
+import java.util.Scanner;
+
 public class Lsh16 {
+	static String name;
+	static int num;
+
+	public static int numbering(int a) {
+
+		a += 5;
+
+		return a;
+
+	}
+
+	public static String firstmyname(String first, String last) {
+		String mix = first + last;
+
+		return mix;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("깃 테스트 메세지");
+
+		int age;
+		
+		System.out.println(name);
+		System.out.println(num);
+
+		int k = numbering(5);
+
+		System.out.println(k);
+
+		String fullname = firstmyname("이", "상화");
+		System.out.println(fullname);
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("나이?");
+		age = sc.nextInt();
+		
+		System.out.println(age);
 	}
 
 }
